f7927c3a21d65a0cb65a98aa4127ab3c8f85e075
em.kim <em.kim@navercorp.com>
2020-11-12 15:17:53 +0900

    circe

--- src/main/scala/json/ParseAny.scala
+++ src/main/scala/json/ParseAny.scala
@@ -1,6 +1,7 @@
 package json
 import cats.syntax.functor._
-import io.circe.Decoder
+import io.circe
+import io.circe.{Decoder, Json}
 import io.circe.generic.auto._
 import io.circe.parser._
 
@@ -10,6 +11,7 @@ sealed trait Item {
 case class ItemInt(name: String, key: Int) extends Item
 case class ItemString(name: String, key: String) extends Item
 case class ItemArray(name: String, key: List[Int]) extends Item
+case class ItemJson(name: String, key: Json) extends Item
 
 object ParseAny extends App {
 
@@ -31,16 +33,42 @@ object ParseAny extends App {
       |	"key" : [1, 2, 3]
       |}""".stripMargin
 
+  val typeJson =
+    """{
+      |	"name" : "json",
+      |	"key" : {
+      |   "a" : {
+      |     "b" : 2
+      |   }
+      | }
+      |}""".stripMargin
+
   implicit val decodeItem: Decoder[Item] =
     List[Decoder[Item]](
       Decoder[ItemInt].widen,
       Decoder[ItemString].widen,
       Decoder[ItemArray].widen,
+      Decoder[ItemJson].widen,
     ).reduceLeft(_ or _)
 
   val parseInt = decode[Item](typeInt)
-  println(parseInt)
+  printValue(parseInt)
 
   val parseArray = decode[Item](typeArray)
-  println(parseArray)
+  printValue(parseArray)
+
+  val parseJson = decode[Item](typeJson)
+  printValue(parseJson)
+
+  def printValue(item: Either[circe.Error, Item]) = {
+    item.foreach { json =>
+      json match {
+        case ItemInt(key, value) => println(value)
+        case ItemString(key, value) => println(value)
+        case ItemArray(key, value) => println(value)
+        case ItemJson(key, value) => println(value \\ "a")
+      }
+    }
+  }
+
 }
